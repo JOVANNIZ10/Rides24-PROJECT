71883c9ddea04dc9339c8151fecd9b28bf0c9a16
ehu <ehu@158.227.124.29>
2025-10-10 12:52:19 +0200

    aldaketak

--- src/test/java/tests/createBalorazioWhiteTestBD.java
+++ src/test/java/tests/createBalorazioWhiteTestBD.java
@@ -145,14 +145,13 @@ public class createBalorazioWhiteTestBD {
             b = sut.createBalorazio(idBalorazioa, puntuazioa, komentarioa, data, travelerNAN, rideNumber);
             sut.close();
 
-            assertNotNull(b);
+            assertNull(b);
 
             testDA.open();
             exist = testDA.existBalorazio(idBalorazioa, puntuazioa, komentarioa, data, travelerNAN, rideNumber);
-            assertTrue(exist);
+            assertTrue(!exist);
             testDA.close();
             
-            fail();
         } catch (reviewAlreadyExistsException e) {
         	assertTrue(true);
         } catch (ratingMoreThanFiveException e) {
